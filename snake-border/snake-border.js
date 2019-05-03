a7beecc7f56728a0dae29db3e4f988407f389597
ireneo-cobarjr <ireneo.cobar.jr@gmail.com>
2019-05-03 20:14:01 +0800

    Code changes for efficiency.

--- snake-border/snake-border.js
+++ snake-border/snake-border.js
@@ -1,170 +1,140 @@
-const snakeBorder = {
-    set: function (param = {}) {
-        var args = {
-            elem   : 'snake-border',
-            color1 : '#757575',
-            color2 : '#424242',
-            width  : 2,
-            speed  : 2
-        } 
-
-        Object.assign(args, param)
-        snakeBorder(args);
-
-        function snakeBorder( arg = defaultSnakeBorder ) {
-            if ( arg.elem.startsWith('#') ) {
-                snakeBorderApplyStyle(arg, document.querySelector(`${arg.elem}`))
-        
-            } else {
-                Array.prototype.forEach.call(document.getElementsByClassName(`${arg.elem}`), el => {
-                    snakeBorderApplyStyle(arg, el)
-                })
-            }
+function __AddClass (x, className) {
+    var name, arr;
+    if (x.classList) {
+        x.classList.add(className);
+    } else {
+        name = className;
+        arr = x.className.split(" ");
+        if (arr.indexOf(name) == -1) {
+            x.className += " " + name;
         }
-        
-        function snakeBorderApplyStyle (ApplyStyle, e) {
-            addSnakeBorderClass(e);
-            for (var snakes = 4; snakes > 0; snakes--) {
-                var s = document.createElement('div')
-                s.classList.add(`snake-${snakes}`)
-                var delay = ApplyStyle.speed / 2;
-        
-                    switch (snakes) {
-                        case 4:
-                        s.setAttribute('style', `width:${ApplyStyle.width}px;animation-duration:${ApplyStyle.speed}s;animation-delay:${delay}s`);
-                        s.setAttribute('snake-head-color', ApplyStyle.color2)
-                        s.setAttribute('snake-tail-color', ApplyStyle.color1)
-                        s.style.background = `linear-gradient(to top, ${ApplyStyle.color1},${ApplyStyle.color2})`
-                        break;
-        
-                        case 3:
-                        s.setAttribute('style', `height: ${ApplyStyle.width}px;animation-duration:${ApplyStyle.speed}s;`);
-                        s.setAttribute('snake-head-color', ApplyStyle.color2)
-                        s.setAttribute('snake-tail-color', ApplyStyle.color1)
-                        s.style.background = `linear-gradient(to left, ${ApplyStyle.color1},${ApplyStyle.color2})`
-                        break;
-        
-                        case 2:
-                        s.setAttribute('style', `width:${ApplyStyle.width}px;animation-duration:${ApplyStyle.speed}s;animation-delay:${delay}s`);
-                        s.setAttribute('snake-head-color', ApplyStyle.color2)
-                        s.setAttribute('snake-tail-color', ApplyStyle.color1)
-                        s.style.background = `linear-gradient(to bottom, ${ApplyStyle.color1},${ApplyStyle.color2})`
-                        break;
-        
-                        case 1:
-                        s.setAttribute('style', `height: ${ApplyStyle.width}px;animation-duration:${ApplyStyle.speed}s;`);
-                        s.setAttribute('snake-head-color', ApplyStyle.color2)
-                        s.setAttribute('snake-tail-color', ApplyStyle.color1)
-                        s.style.background = `linear-gradient(to right, ${ApplyStyle.color1},${ApplyStyle.color2})`
-                        break;
-                    }
-                e.insertBefore(s, e[0])
-            }
+    } 
+}
+
+function __RemoveClass (x, className) {
+    var name, arr;
+    if (x.classList) {
+        x.classList.remove(className);
+    } else {
+        name = className;
+        arr = x.className.split(" ");
+        if (arr.indexOf(name) != -1) {
+            x.replace(name, '')
         }
-        
-        function addSnakeBorderClass (x) {
-            var name, arr;
-            if (x.classList) {
-                x.classList.add("snake-border");
-            } else {
-                name = "snake-border";
-                arr = x.className.split(" ");
-                if (arr.indexOf(name) == -1) {
-                    x.className += " " + name;
-                }
-            } 
-        }
-        
-    },
-    unSet: function (elem) {
-        if ( elem.startsWith('#') ) {
-            removeStyle(document.querySelector(elem))
-    
-        } else {
-            Array.prototype.forEach.call(document.getElementsByClassName(elem), el => {
-                removeStyle(el)
-            })
-        }
+    } 
+}
 
-        function removeStyle (e) {
-            for (x = 4; x > 0; x--) {
-                var c = e.querySelector(`.snake-${x}`)
-                if (c) {
-                    e.removeChild(c)
-                }
-            }
+function __Snake(args) {
+    this.pt = args.pt
+    this.pn = args.pn
 
-            var name, arr;
-            if (e.classList) {
-                e.classList.remove("snake-border");
-            } else {
-                name = "snake-border";
-                arr = e.className.split(" ");
-                if (arr.indexOf(name) != -1) {
-                    e.replace(name, '')
-                }
-            } 
-        }
+    this.instance = document.createElement('div')
+    __AddClass(this.instance, `snake-${this.pn}`)
 
-    },
-    changeColor: function (param = {}) {
-        var arg = {
-            elem   : 'snake-border',
-            color1 : 'no_value',
-            color2 : 'no_value',
-            width  : -1,
-            speed  : -1
-        }
+    switch (this.pn) {
+        case 1:
+        this.w = 'height'
+        this.dn = 'right'
+        break;
 
-        Object.assign(arg, param)
+        case 2:
+        this.w = 'width'
+        this.dn = 'bottom'
+        break;
 
-        if ( arg.elem.startsWith('#') ) {
-            changeStyle(document.querySelector(`${arg.elem}`))
-    
-        } else {
-            Array.prototype.forEach.call(document.getElementsByClassName(`${arg.elem}`), el => {
-                changeStyle(el)
-            })
-        }
+        case 3:
+        this.w = 'height'
+        this.dn = 'left'
+        break;
 
-
-        function changeStyle (e) {
-            if (arg.color1 != 'no_value') {
-                for (x = 4; x > 0; x--) {
-                    var snake = e.querySelector(`.snake-${x}`)
-                    snake.setAttribute('snake-tail-color', arg.color1)
-                }
-            }
-            if (arg.color2 != 'no_value') {
-                for (x = 4; x > 0; x--) {
-                    var snake = e.querySelector(`.snake-${x}`)
-                    snake.setAttribute('snake-head-color', arg.color2)
-                }
-            }
-            for (y = 4; y > 0; y--) {
-                var snake = e.querySelector(`.snake-${y}`)
-                switch (y) {
-                    case 4:
-                    snake.style.background = `linear-gradient(to top, ${snake.getAttribute('snake-tail-color')},${snake.getAttribute('snake-head-color')})`
-                    break;
-
-                    case 3:
-                    snake.style.background = `linear-gradient(to left, ${snake.getAttribute('snake-tail-color')},${snake.getAttribute('snake-head-color')})`
-                    break;
-
-                    case 2:
-                    snake.style.background = `linear-gradient(to bottom, ${snake.getAttribute('snake-tail-color')},${snake.getAttribute('snake-head-color')})`
-                    break;
-
-                    case 1:
-                    snake.style.background = `linear-gradient(to right, ${snake.getAttribute('snake-tail-color')},${snake.getAttribute('snake-head-color')})`
-                    break;
-                }
-            }
-
-        }
+        case 4:
+        this.w = 'width'
+        this.dn = 'top'
+        break;
     }
+
+    this.setColor = function (c1,c2) {
+        (this.instance).style.background = `linear-gradient(to ${this.dn}, ${c1},${c2})`
+    }
+    this.setWidth = function (w) {
+        (this.instance).style[`${this.w}`] = `${w}px`
+    }
+    this.setSpeed = function (s) {
+        if (this.pn == 4 || this.pn == 2) {(this.instance).style.animationDelay = `${s / 2}s`}
+        (this.instance).style.animationDuration = `${s}s`
+    }
+
+    this.setColor(args.tail, args.head);
+    this.setWidth(args.width);
+    this.setSpeed(args.sd);
+    (this.pt).insertBefore(this.instance,(this.pt)[0]);
+
 }
 
 
-//On the road features would be changing properties, setting distance from the element and removing snakeBorders dynamically.
+function SnakeBorder(param = {}) {
+    var args = {
+        elem   : 'snake-border',
+        color1 : '#757575',
+        color2 : '#424242',
+        width  : 2,
+        speed  : 2
+    } 
+
+    Object.assign(args, param)
+
+    this.head = args.color2;
+    this.tail = args.color1;
+    this.w = args.width;
+    this.s = args.speed;
+    this.snakes = [];
+
+    // Needs to add check and then throw error if args.elem is invalid 
+    ( args.elem.startsWith('#')) ?
+        (
+            this.el = document.querySelector(args.elem),
+            this.access = function (cb) { cb(this.el) }
+        )
+        :
+        (
+            this.el = document.getElementsByClassName(args.elem),
+            this.access = function (cb) {
+                Array.prototype.forEach.call(this.el, e => {cb(e)})
+            }
+        )
+
+    this.access( e => {
+        for (var snakes = 4; snakes > 0; snakes--) {
+            let snake = new __Snake({
+                pt   : e,
+                pn   : snakes,
+                tail : this.tail,
+                head : this.head,
+                sd   : this.s,
+                width: this.w
+            })
+            this.snakes.push(snake)
+        }
+        __AddClass(e,'snake-border')
+    })
+
+    this.destroy = function () {
+        this.access(e => {
+            this.snakes.forEach(s => {
+                e.removeChild(s.instance)
+            })
+            __RemoveClass(e, 'snake-border')
+        })
+    }
+
+    this.changeColor = function(a = {}) {
+        var d = {tail : this.tail, head : this.head}
+        Object.assign(d, a)
+
+        this.access(e => {
+            this.snakes.forEach(s => {
+                s.setColor(d.tail,d.head)
+            })
+        })
+    }
+}
