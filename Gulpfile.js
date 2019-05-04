b342ca865303b8529eda5c400d47db077f6768ea
ireneo-cobarjr <ireneo.cobar.jr@gmail.com>
2019-05-04 23:11:47 +0800

    Added watchers, for development and building scss files.

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,11 +6,21 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const terser = require('gulp-terser');
 const rename = require("gulp-rename");
-
-gulp.task('clean', () => del(['dist']));
+var sass = require('gulp-sass');
  
+sass.compiler = require('node-sass');
+
+///////////////////////////////////////////////////////
+// Cleaning utilities
+gulp.task('clean', () => del(['dist']));
+gulp.task('snake-border-clean', () => del(['dist/snake-border']));
+///////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////
+// Snake Border
 gulp.task('minify-snake-border-css', () => {
-    return gulp.src('snake-border/snake-border.css')
+    return gulp.src('snake-border/snake-border.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -27,12 +37,56 @@ gulp.task('minify-snake-border-js', function() {
       .pipe(gulp.dest('dist/snake-border'))
 });
 
-  gulp.task('default',
-    gulp.series(
-        'clean',
+gulp.task('watch-snake-border-scss', function () {
+    return gulp.watch('snake-border/snake-border.scss', gulp.series('minify-snake-border-css'));
+});
+gulp.task('watch-snake-border-js', function () {
+    return gulp.watch('snake-border/snake-border.js', gulp.series('minify-snake-border-js'));
+});
+
+    gulp.task('snake-border',
         gulp.parallel(
-            'minify-snake-border-css',
-            'minify-snake-border-js'
+            'minify-snake-border-js',
+            gulp.series(
+                'minify-snake-border-css'
+            )
         )
-    )
-  );
+    );
+///////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////
+// Development Build
+    gulp.task('snake-border-dev',
+        gulp.series(
+            'snake-border-clean',
+            gulp.parallel(
+                gulp.series(
+                    'minify-snake-border-js',
+                    'watch-snake-border-js'
+                ),
+                gulp.series(
+                    'minify-snake-border-css',
+                    'watch-snake-border-scss'
+                )
+            )
+        )
+    );
+
+    gulp.task('dev',
+        gulp.parallel(
+            'snake-border-dev'
+        )
+    );
+///////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////
+// Release Build
+    gulp.task('default',
+        gulp.series(
+            'clean',
+            gulp.parallel(
+                'snake-border'
+            )
+        )
+    );
+///////////////////////////////////////////////////////
